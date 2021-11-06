13aed1cca8d99a5e857a18e39c81063b249d7275
vannguyen <vannguyen599@u.boisestate.edu>
2021-11-06 15:03:32 -0600

    fix(work flow): update

--- release.config.js
+++ release.config.js
@@ -9,19 +9,13 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md'
-      }
-    ],
-    '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'dist/**'],
-        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
+    '@semantic-release/changelog',
+    ["@semantic-release/npm", { "npmPublish": false}],
+    ["@semantic-release/git",
+    {
+      "assets": ['CHANGELOG.md', 'dist/**'],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    "@semantic-release/github"
   ]
 }
