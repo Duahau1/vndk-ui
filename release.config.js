8cbb3f97734fea1f4c89bd7675b97ae46738dac2
vannguyen <vannguyen599@u.boisestate.edu>
2021-11-06 16:59:58 -0600

    fix(fix): fix
    fix

--- release.config.js
+++ release.config.js
@@ -10,13 +10,13 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    "@semantic-release/npm",
     ["@semantic-release/git",
     {
       "assets": ['package.json', 'dist/**'],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    "@semantic-release/github",
-    "@semantic-release/npm",
+    "@semantic-release/github"
 
   ]
 }
